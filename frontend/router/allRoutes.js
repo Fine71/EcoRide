420e66e03df31ee1dde7775c994e7d2819a69118
Delphine Esparon <delphine.esparon@gmail.com>
2025-11-20 11:03:15 +0100

    Ajout de la partie recherche + ajout de la page covoiturage

--- frontend/router/allRoutes.js
+++ frontend/router/allRoutes.js
@@ -4,6 +4,7 @@ import Route from "./route.js";
 
 export const allRoutes = [
     new Route("/", "Accueil", "/frontend/pages/home.html"),
+    new Route("/covoiturage", "Covoiturage", "/frontend/pages/covoiturage.html"),
 ];
 
 // affichage du titre comme ceci : Route.title - WebsiteName
