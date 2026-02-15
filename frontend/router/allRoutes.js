1195f0e38425015cd94798bcfa9a51589911f879
Fine71 <delphine.esparon@gmail.com>
2026-02-15 10:07:18 +0100

    Mise à jour dossier git

--- frontend/router/allRoutes.js
+++ frontend/router/allRoutes.js
@@ -3,7 +3,10 @@ import Route from "./Route.js";
 // Définition des routes de l'application
 
 export const allRoutes = [
-    new Route("/", "Accueil", "/Pages/Home.html"),
+    new Route("/", "Accueil", "/pages/home.html", []),
+    new Route("/signin", "Connexion", "/pages/connexion.html", []),
+    new Route("/contact", "Contact", "/pages/contact.html", []),
+    new Route("/services", "Services", "/pages/services.html", []),
 ];
 
 // affichage du titre comme ceci : Route.title - WebsiteName
--- frontend/router/router.js
+++ frontend/router/router.js
@@ -1,8 +1,8 @@
-import Route from "./Route.js";
+import Route from "./route.js";
 import { allRoutes, websiteName } from "./allRoutes.js";
 
 // Création d'une route pour la page 404 (page introuvable)
-const route404 = new Route("404", "Page introuvable", "/Pages/404.html");
+const route404 = new Route("404", "Page introuvable", "/Pages/404.html",[]);
 
 // Fonction pour récupérer la route correspondant à une URL donnée
 const getRouteByUrl = (url) => {
