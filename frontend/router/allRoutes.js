6f1933977ebd9b71772165dfb6d61a644fc2aa6b
Fine71 <delphine.esparon@gmail.com>
2026-02-16 19:54:59 +0100

    edit page connexion et inscription

--- frontend/router/allRoutes.js
+++ frontend/router/allRoutes.js
@@ -1,11 +1,12 @@
-import Route from "./Route.js";
+import Route from "./route.js";
 
 // Définition des routes de l'application
 
 export const allRoutes = [
     new Route("/", "Accueil", "/pages/home.html", []),
     new Route("/covoiturage", "Covoiturage", "/pages/covoiturage.html", []),
-    new Route("/signin", "Connexion", "/pages/signin.html", []),
+    new Route("/signin", "Connexion", "/pages/auth/signin.html", ["disconnected"]),
+    new Route("/signup", "Inscription", "/pages/auth/signup.html", ["disconnected"]),
     new Route("/contact", "Contact", "/pages/contact.html", []),
     new Route("/services", "Services", "/pages/services.html", []),
 ];
